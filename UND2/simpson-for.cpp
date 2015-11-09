5427ca4db108631e7d01fa72a1207dff67244f00
Testeitor <et_malibu@hotmail.com>
2015-11-09 19:06:51 +0100

    while y for

---
+++ UND2/simpson-for.cpp
@@ -0,0 +1,33 @@
+//
+//
+// Hacer un programa que repita 10 veces "No debo copiar en clase" by Bart Simpson
+// Empleando un bucle for.
+//
+
+#include <iostream>
+using namespace std;
+
+int main(){
+
+
+		//0.Declaración de Variables
+
+		int contador = 0;
+
+
+
+		//1.Empleamos un bucle for para que se repita el mensaje "No debo copiar en clase" 10 veces.
+
+
+		for(contador=1;contador<=10;contador++){
+
+
+		cout << "Linea: " << contador << " No debo copiar en clase" << endl;
+
+	
+		}
+
+
+
+
+}//Final de programa
---
+++ UND2/simpsonv2.cpp
@@ -0,0 +1,29 @@
+//
+//
+// Simpson "No debo copiar en clase" (Escribirlo 10 veces)
+//
+//
+
+#include <iostream>
+using namespace std;
+
+int main(){
+
+
+	//0.Declaración de Variables
+
+	int contador = 0;
+
+	//1.Ahora implementamos un bucle while para que aparezca 10 veces "No debo copiar en clase".
+
+	while(contador < 10){
+
+	contador = contador +1;
+
+	cout << "Linea: " << contador << " No debo copiar en clase" << endl;
+
+
+	}
+
+	
+}
